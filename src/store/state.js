2e07123d32d880c79f2b09c34602b1c9d6b10946
Luke Williams <luke@iquidus.io>
2018-08-12 04:29:12 +0200

    add rpc to state

--- src/store/state.js
+++ src/store/state.js
@@ -6,6 +6,7 @@ let state = {
     transactions: 0,
     uncles: 0
   },
-  api: 'http://127.0.0.1:3000/'
+  api: 'http://127.0.0.1:3000/',
+  rpc: 'https://rpc1.ubiqscan.io'
 }
 export default state
