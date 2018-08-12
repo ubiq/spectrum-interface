e5de036aacdd27f87b8ecff76615cc0739021db0
Luke Williams <luke@iquidus.io>
2018-08-12 09:31:16 +0200

    Transactions: active = String -> active = Boolean

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -5,7 +5,7 @@
         <b-breadcrumb>
           <b-breadcrumb-item :to="{name: 'Home'}">Home</b-breadcrumb-item>
           <b-breadcrumb-item :active="type === 'latest'" :to="{name: 'Transactions', params: {type: 'latest'}}">Transactions</b-breadcrumb-item>
-          <b-breadcrumb-item v-if="type === 'pending'" active='true'>Pending</b-breadcrumb-item>
+          <b-breadcrumb-item v-if="type === 'pending'" active>Pending</b-breadcrumb-item>
           <b-breadcrumb-link>
             <b-button :class="{fa: true, 'fa-refresh': true, 'fa-spin': refreshing, 'btn-breadcrumb': true}" v-on:click="fetch()"/>
           </b-breadcrumb-link>
