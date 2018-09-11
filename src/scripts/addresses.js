58525689346a8d7b5fccba0b0d31032669617dd3
Luke Williams <luke@iquidus.io>
2018-09-11 12:20:39 +0200

    addresses: update addresses

--- src/scripts/addresses.js
+++ src/scripts/addresses.js
@@ -19,15 +19,24 @@ var addresses = {
   // Exchanges
   '0xb3c4e9ca7c12a6277deb9eef2dece65953d1c864': 'Bittrex',
   '0x57e0ebfade42211c8a4837ad1bb9c1c290ee265b': 'Einstein',
+  // Other
   '0x2300b30dfd91306a5acfa8ae00a3a47d519303ee': 'Infinitum Card',
+  '0xae3f04584446aa081cd98011f80f19977f8c10e0': 'Infinitum Flame',
   // Tokens
   '0xd245207cfbf6eb6f34970db2a807ab1d178fde6c': 'APX',
+  '0xff3bf057adf3b0e015b6465331a6236e55688274': 'BeerToken',
+  '0x79755ea6ccb143b0bd8e1b670efffd59ae874991': 'BettingWin',
+  '0x0319b92eb57929684c27531da133b0d2bbf691af': 'Cauli',
   '0x08533d6a06ce365298b12ef92eb407cba8aa8273': 'CryptopiaFeeShare',
   '0x94ad7e41c1d44022c4f47cb1ba019fd1a022c536': 'Dotcoin',
   '0xcf3222b7fda7a7563b9e1e6c966bead04ac23c36': 'Escher',
+  '0x105bbb0fd91329bec3f12546cbe7edfe624ec3ad': 'Giveaway',
   '0x500684ce0d4f04abedff3e54fcf8acc5e6cfc4bd': 'GeoCoin',
+  '0x0826180a4c981d5095cb5c48bb2a098a44cf6f73': '10grans',
   '0x4b4899a10f3e507db207b0ee2426029efa168a67': 'Qwark',
-  '0x5e1715bb79805bd672729760b3f7f34d6f485098': 'Ricks',
+  '0x5e1715bb79805bd672729760b3f7f34d6f485098': 'PickleRicks',
+  '0x497e20586f86c35592ff8f65cde94f296514c387': 'Snarg01',
+  '0xbc3e373d2b24faf1dfe5b3f3acff9d66a7b48869': 'WorldBit Token',
   // Contracts
   '0x256009e4687ab2183bbca3e4ea54ba6f0a7ad21e': 'ESCH Airdrop #1',
   '0x36ae3bdbbd19bc55f2fe3cccf41bf12c4c3afb42': 'ESCH Airdrop #2',
