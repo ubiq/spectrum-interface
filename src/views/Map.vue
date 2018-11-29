d2023e1e6411b4a8f77c3d2f60c1fae0a7e1e442
Angel Lox <AngelloxBit@gmail.com>
2018-11-29 14:58:14 +0100

    Rework Stats with css-grid

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,20 +1,22 @@
 <template>
-<b-card>
-  <b-row class="justify-content-md-center">
-    <b-col md="2">
-      <h5>Nodes <small>{{data.length}}</small></h5>
-      <b-list-group>
-        <b-list-group-item v-for="v in this.stats" :key="v[0]" class="d-flex justify-content-between align-items-center">
-          <span>{{v[0]}}</span>
-          <b-badge variant="primary" pill>{{v[1]}}</b-badge>
-        </b-list-group-item>
-      </b-list-group>
-    </b-col>
-    <b-col md="8" offset-md="2">
-      <nodemap :locations="data"></nodemap>
-    </b-col>
-  </b-row>
-</b-card>
+  <b-card>
+    <b-row class="justify-content-md-center">
+      <b-col md="2">
+        <h5>Nodes <small>{{data.length}}</small></h5>
+        <b-list-group>
+          <b-list-group-item v-for="v in this.stats" :key="v[0]" class="d-flex justify-content-between align-items-center">
+            <span>{{v[0]}}</span>
+            <b-badge variant="primary" pill>{{v[1]}}</b-badge>
+          </b-list-group-item>
+        </b-list-group>
+      </b-col>
+      <b-col md="6" offset-md="2">
+        <b-card header="Node Map" body-bg-variant="#222222">
+          <nodemap :locations="data"></nodemap>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-card>
 </template>
 <script>
 import map from '../components/maps/Nodemap'
--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,40 +1,29 @@
 <template>
-<b-card no-body>
-  <b-tabs pills card vertical>
-    <b-tab title="Tab 1" active>
-      <template slot="title">
-          <span @click="tab('difficulty')">Difficulty</span>
-      </template>
-      <div class="chart-container">
-        <difficulty v-if="this.tabs.difficulty === true"/>
+  <div class="">
+    <b-card style="height:700px;" no-body>
+      <div slot="header">
+        {{this.text[this.isActive]}}
       </div>
-    </b-tab>
-    <b-tab title="Tab 2">
-      <template slot="title">
-          <span @click="tab('avggas')">Avg. gas</span>
-      </template>
-      <div class="chart-container">
-        <avggastx v-if="this.tabs.avggas === true"/>
+      <div class="testcontainer" :class="classObject">
+        <a @click="active('a')" :class="isActive === 'a' ? 'a ' + 'active' : 'a'">
+          <b-button @click="isActive = 'a'" v-if="this.isActive !== 'a'" name="button">{{this.text.a}}</b-button>
+          <difficulty v-else/>
+        </a>
+        <a @click="active('b')" :class="isActive === 'b' ? 'b ' + 'active' : 'b'">
+          <b-button @click="isActive = 'b'" v-if="this.isActive !== 'b'" name="button">{{this.text.b}}</b-button>
+          <avggastx v-else/>
+        </a>
+        <a @click="active('c')" :class="isActive === 'c' ? 'c ' + 'active' : 'c'">
+          <b-button @click="isActive = 'c'" v-if="this.isActive !== 'c'" name="button">{{this.text.c}}</b-button>
+          <blocktime v-else/>
+        </a>
+        <a @click="active('d')" :class="isActive === 'd' ? 'd ' + 'active' : 'd'">
+          <b-button @click="isActive = 'd'" v-if="this.isActive !== 'd'" name="button">{{this.text.d}}</b-button>
+          <blocktime88 v-else/>
+        </a>
       </div>
-    </b-tab>
-    <b-tab title="Tab 3">
-      <template slot="title">
-          <span @click="tab('blocktime')">Blocktime</span>
-      </template>
-      <div class="chart-container">
-        <blocktime v-if="this.tabs.blocktime === true"/>
-      </div>
-    </b-tab>
-    <b-tab title="Tab 4">
-      <template slot="title">
-          <span @click="tab('blocktime88')">BT - 88</span>
-      </template>
-      <div class="chart-container">
-        <blocktime88 v-if="this.tabs.blocktime88 === true"/>
-      </div>
-    </b-tab>
-  </b-tabs>
-</b-card>
+    </b-card>
+  </div>
 </template>
 <script>
 import avggastx from './charts/avggastx'
@@ -52,43 +41,100 @@ export default {
   },
   data () {
     return {
-      tabs: {
-        'difficulty': false,
-        'avggas': false,
-        'blocktime': false,
-        'blocktime88': false
+      isActive: "",
+      text: {
+        'a':'Difficulty/Hashrate',
+        'b':'Avg.gas/Gaslimit/Txns',
+        'c':'Avg.blocktime',
+        'd':'Avg.88-block period blocktime'
       },
       refresh: false
     }
   },
-  created () {
-    this.tabs.difficulty = true
-    // let self = this;
-    // (function loop () {
-    //   self.fetch()
-    //   setTimeout(loop, 5000)
-    // })()
-    // console.log(this.chartOptions, this.chartData)
-  },
   methods: {
-    /*
-    Chartjs + the b-tab component acts up. If the element containing the chart is hidden the chart is rendered with height 0.
-    We pass a prop to each chart view that triggers a v-if every time we select a tab, and we listen to an event from the chart views
-    to reset the refresh var.
-    */
-    update () {
-      console.log('stats: refresh!', this.refresh)
-      this.refresh = true
-    },
-    tab (i) {
-      console.log(i)
-      this.tabs[i] = true
+    active (x) {
+      this.isActive = x
+    }
+  },
+  computed: {
+    classObject: function() {
+      let str = "gridlayout-" + this.isActive
+      return str
     }
   }
 }
 </script>
 <style lang="scss" scoped>
+
+@mixin active_panel($x) {
+
+  $vars: "a", "b", "c", "d";
+
+  $str: "";
+
+  @each $item in $vars {
+    @if $item != $x {
+      $str: #{$str + " " + $item + " " + $item}
+    }
+  }
+
+  grid-template-areas:
+  "#{$x} #{$x} #{$x} #{$x} #{$x} #{$x}"
+  "#{$x} #{$x} #{$x} #{$x} #{$x} #{$x}"
+  "#{$x} #{$x} #{$x} #{$x} #{$x} #{$x}"
+  "#{$str}";
+
+}
+
 .chart-container {
   width: 1600px;
 }
+
+.testcontainer {
+  display: grid;
+  height: 100%;
+  grid-template-rows: 50px 1fr 1fr 50px;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-areas:
+  ". . . . . ."
+  ". a a b b ."
+  ". c c d d ."
+  ". . . . . .";
+}
+
+.gridlayout-a {
+  @include active_panel("a");
+}
+.gridlayout-b {
+  @include active_panel("b");
+}
+.gridlayout-c {
+  @include active_panel("c");
+}
+.gridlayout-d {
+  @include active_panel("d");
+}
+
+.a {
+  grid-area: a;
+}
+.b {
+  grid-area: b;
+}
+.c {
+  grid-area: c;
+}
+.d {
+  grid-area: d;
+}
+
+.btn {
+  height: 100%;
+  width: 100%;
+  border: 0;
+  &:hover {
+    background-color: #212121;
+  }
+}
+
 </style>
